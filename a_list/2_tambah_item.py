e651374d2f379d680c7941ae76326329ac8a91d7
iiohanestj09 <putra.dae2005@gmail.com>
2025-09-02 10:51:44 +0700

    Update a.2 - rapihin kode

--- a_list/2_tambah_item.py
+++ a_list/2_tambah_item.py
@@ -4,17 +4,19 @@ list1.append("watermelon")
 list1.append("banana")
 print(list1)
 
+
 # B. Menggunakan .insert()  -> menambah item sesuai index
 list1.insert(0, "grape")
 list1.insert(-1, "strawberry")    # hasil akhir indexnya bukan [-1] tapi [-2], karena item asli [-1] bergeser ke kanan 
 print(list1)
+
 
 # C. Menggunakan .extend()  -> menambah banyak item di akhir
 list2 = ["monkey", "elephant", "bird"]
 list1.extend(list2)
 print(list1)
 
-# .extend() dapat menambah banyak item selain list, bisa set, tupple, dictionary dll (iterable items)
+  # .extend() dapat menambah banyak item selain list, bisa set, tupple, dictionary dll (iterable items)
 dict1 = {
     "greet": "haloo",
     "nama": "Putra",
