069172065e0a0ef2000d826e06d805d5660242db
iiohanestj09 <putra.dae2005@gmail.com>
2025-09-02 20:31:38 +0700

    Add c.10 - pengenalan_set

---
+++ c_set/10_pengenalan_set.py
@@ -0,0 +1,14 @@
+# Set -> unordered, unchangeable, unindexed, no duplicate
+# walau unchangeable, set bisa remove/pop, add, update dll
+# unordered, output dari set tidak beraturan (kadang terurut kadang tidak), karena set bukan urutan inputan dan tidak bisa diandalkan
+#! Note: nilai True dan 1 Sama  |  nilai False dan 0 sama di Set, sehingga salah satunya akan disimpan
+
+# Deklarasi Set
+set1 = {3, 0, True, False, 2, 1.1, 2.2, 1.1, 3}
+print(set1)
+print(type(set1))
+print(len(set1))
+
+set2 = set(("ayam", "cumi", 2, True, "kambing", "ayam", 2))
+print(set2)
+print(len(set2))
