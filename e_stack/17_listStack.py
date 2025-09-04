0b692b1d9a507b278b2598f909c2f2f000c654be
iiohanestj09 <putra.dae2005@gmail.com>
2025-09-04 18:15:07 +0700

    Add e.17 - listStack

---
+++ e_stack/17_listStack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        if len(self.items) == 0:
+            print("Stack is empty!")
+            return None
+        poppedItem = self.items.pop()
+        return poppedItem
+    
+    def peek(self):
+        if len(self.items) == 0:
+            print("Stack is empty!")
+            return None
+        return self.items[-1]
+    
+    def display(self):
+        if len(self.items) == 0: print("Stack is Empty!")
+        print(f"{self.items[::-1]}")     # list[start:stop:step]
+
+
+# Implementasi
+st = Stack()
+st.push(10)
+st.push(20)
+st.push(30)
+
+st.pop()
+st.push(40)
+
+print("Peek:", st.peek())
+st.display()
