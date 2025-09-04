51117d5824498a435aa0cf7f3f8b83ed997cb2db
iiohanestj09 <putra.dae2005@gmail.com>
2025-09-04 18:32:02 +0700

    Add e.18 - dequeStack

---
+++ e_stack/18_dequeStack.py
@@ -0,0 +1,43 @@
+# Menggunakan collections.deque  -> lebih efisien
+
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.items = deque()
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        if len(self.items) == 0:
+            print("Stack is empty!")
+            return None
+        return self.items.pop()
+    
+    def peek(self):
+        if len(self.items) == 0: 
+            print("Stack is empty!")
+            return None
+        return self.items[-1]
+    
+    def display(self):
+        if len(self.items) == 0: print("Stack is empty!")
+        else: print(list(reversed(self.items)))
+
+
+# Implementation
+st = Stack()
+
+print(st.pop())
+st.display()
+
+st.push(10)
+st.push(20)
+st.push(30)
+
+print(st.pop())
+st.push(40)
+
+print("Peek:", st.peek())
+st.display()
